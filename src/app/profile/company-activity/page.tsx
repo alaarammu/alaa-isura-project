dd14dade1ed4c3cc8ced7a18e6b25b65ac45ff06
Alaa Rammu <160013433+alaarammu@users.noreply.github.com>
2024-05-20 12:58:27 +0100

    fixing-sizes

--- src/app/profile/company-activity/page.tsx
+++ src/app/profile/company-activity/page.tsx
@@ -5,9 +5,11 @@ export default function CompanyActivity() {
   return (
     <div className="bg-purple-950 min-h-screen flex flex-col items-start pt-9 pl-9 pr-9 pb-9">
       <div className="flex space-x-9 items-start">
-        <FaUserCircle className="text-9xl text-purple-400 bg-white avatar aspect-square rounded-full" />
+        <div className="relative w-36 h-36 flex-shrink-0">
+          <FaUserCircle className="absolute inset-0 text-purple-400 bg-white rounded-full w-full h-full" />
+        </div>
 
-        <div className="bg-white rounded-xl shadow-lg p-6">
+        <div className="bg-white rounded-xl shadow-lg p-8">
           <h1 className="text-purple-950 font-bold pl-4 pr-4 pb-1">Username</h1>
           <p className="text-purple-500 pl-4 pr-4 pb-1">AlaaRammu</p>
           <p className="text-purple-500 pl-4 pr-4">
@@ -17,27 +19,27 @@ export default function CompanyActivity() {
 
         <div className="bg-purple-950 rounded-full p-6 mt-9 flex space-x-6">
           <Link href="./likes" legacyBehavior>
-            <a className="text-white font-bold rounded-full pt-3 pb-3 bg-purple-950 hover:bg-purple-500 focus:bg-purple-500 pr-3 pl-3">
+            <a className="text-white font-bold rounded-full pt-3 pb-3 bg-purple-950 hover:bg-purple-500 focus:bg-purple-500 pr-3 pl-3 flex-shrink-0">
               Likes
             </a>
           </Link>
           <Link href="./questions" legacyBehavior>
-            <a className="text-white font-bold rounded-full pt-3 pb-3 bg-purple-950 hover:bg-purple-500 focus:bg-purple-500 pr-3 pl-3">
+            <a className="text-white font-bold rounded-full pt-3 pb-3 bg-purple-950 hover:bg-purple-500 focus:bg-purple-500 pr-3 pl-3 flex-shrink-0">
               My Questions
             </a>
           </Link>
           <Link href="./answers" legacyBehavior>
-            <a className="text-white font-bold rounded-full pt-3 pb-3 bg-purple-950 hover:bg-purple-500 focus:bg-purple-500 pr-3 pl-3">
+            <a className="text-white font-bold rounded-full pt-3 pb-3 bg-purple-950 hover:bg-purple-500 focus:bg-purple-500 pr-3 pl-3 flex-shrink-0">
               My Answers
             </a>
           </Link>
           <Link href="./company-activity" legacyBehavior>
-            <a className="text-white font-bold rounded-full pt-3 pb-3 bg-purple-500 hover:bg-purple-500 focus:bg-purple-500 pr-3 pl-3">
+            <a className="text-white font-bold rounded-full pt-3 pb-3 bg-purple-500 hover:bg-purple-500 focus:bg-purple-500 pr-3 pl-3 flex-shrink-0">
               Company Activity
             </a>
           </Link>
           <Link href="./manage-users" legacyBehavior>
-            <a className="text-white font-bold rounded-full pt-3 pb-3 bg-purple-950 hover:bg-purple-500 focus:bg-purple-500 pr-3 pl-3">
+            <a className="text-white font-bold rounded-full pt-3 pb-3 bg-purple-950 hover:bg-purple-500 focus:bg-purple-500 pr-3 pl-3 flex-shrink-0">
               Manage Users
             </a>
           </Link>
