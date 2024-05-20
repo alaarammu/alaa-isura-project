cf14e16ec42bb5ea1af1a15e6dc50809201ee078
Alaa Rammu <160013433+alaarammu@users.noreply.github.com>
2024-05-20 14:33:53 +0100

    login-dropdown

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,12 +2,20 @@
 import { CiBowlNoodles } from "react-icons/ci";
 import Link from "next/link";
 import { IoSearch } from "react-icons/io5";
+import { useState } from "react";
+import { IoMdArrowDropdown } from "react-icons/io";
 
 export default function Header() {
+  const [dropdownVisible, setDropdownVisible] = useState(false);
+
   const handleKeyPress = (event) => {
     if (event.key === "Enter") {
       console.log("Search triggered");
     }
+  };
+
+  const toggleDropdown = () => {
+    setDropdownVisible(!dropdownVisible);
   };
 
   return (
@@ -29,12 +37,23 @@ export default function Header() {
         />
         <IoSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-purple-500 text-2xl" />
       </div>
-      <div className="flex items-center space-x-4">
-        <Link legacyBehavior href="">
-          <a className="text-white font-semibold font-calibri transition duration-300 cursor-pointer hover:text-purple-500 text-lg">
+      <div className="flex items-center space-x-4 relative">
+        <div className="relative">
+          <a
+            className="text-white font-semibold font-calibri transition duration-300 cursor-pointer hover:text-purple-500 text-lg flex items-center"
+            onClick={toggleDropdown}
+          >
             Login
+            <IoMdArrowDropdown className="ml-1" />
           </a>
-        </Link>
+          {dropdownVisible && (
+            <div className="absolute top-full mt-2 right-0 bg-purple-800 text-white font-bold rounded-xl shadow-xl py-2 px-4 z-10 min-w-max">
+              <Link legacyBehavior href="/profile">
+                <a className="block px-4 py-2 hover:bg-purple-700">My Profile</a>
+              </Link>
+            </div>
+          )}
+        </div>
         <span className="block w-px h-6 bg-purple-400"></span>
         <Link legacyBehavior href="">
           <a className="text-white font-semibold font-calibri transition duration-300 cursor-pointer hover:text-purple-500 text-lg">
